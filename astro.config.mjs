fbbd19f3ef8d14e6cc6189aecbd9ccf021602848
HenryWashuHe <he.h@wustl.edu>
2025-10-15 17:10:13 -0400

    fix: add base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://henrywashuhe.github.io',
+  base: '/Personal_Portfolio',
   integrations: [mdx(), sitemap(), tailwind()]
 });
