e8d4a5a53f042c7fcb4ed70be90cd4bd0354a7c5
HenryWashuHe <he.h@wustl.edu>
2025-10-15 16:49:10 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
+  base: '/Personal_Portfolio', // your repo name here
   integrations: [mdx(), sitemap(), tailwind()]
 });
